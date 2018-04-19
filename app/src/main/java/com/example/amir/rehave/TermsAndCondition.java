c7a9e627006f43b296e85c4b71d0e91dde390305
bladyamir <mr.amir.1971@gmail.com>
2018-04-19 12:03:21 +0600

    terms and condition page added

---
+++ app/src/main/java/com/example/amir/rehave/TermsAndCondition.java
@@ -0,0 +1,42 @@
+package com.example.amir.rehave;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+public class TermsAndCondition extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_and_condition);
+        CheckBox tcTick=findViewById(R.id.tc_tick);
+        final Button tcBtn=findViewById(R.id.tc_button);
+        tcTick.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    tcBtn.setBackgroundColor(Color.GREEN);
+                    tcBtn.setOnClickListener(TermsAndCondition.this);
+                }else{
+                    tcBtn.setBackgroundColor(Color.WHITE);
+                    tcBtn.setOnClickListener(null);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(TermsAndCondition.this,MainActivity.class));
+
+    }
+}
