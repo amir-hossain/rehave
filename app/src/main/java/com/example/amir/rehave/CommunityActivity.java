ebdc2d8c0caa0683af206f607bc0fb17cd644da3
bladyamir <mr.amir.1971@gmail.com>
2018-06-27 03:59:13 +0600

    forum disign change

--- app/src/main/java/com/example/amir/rehave/CommunityActivity.java
+++ app/src/main/java/com/example/amir/rehave/CommunityActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -28,6 +29,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class CommunityActivity extends AppCompatActivity implements CommunityListAdapter.ItemClicked {
+
     FloatingActionButton review;
     FloatingActionButton add;
     private static RecyclerView.Adapter adapter;
--- app/src/main/java/com/example/amir/rehave/others/CommunityListAdapter.java
+++ app/src/main/java/com/example/amir/rehave/others/CommunityListAdapter.java
@@ -1,11 +1,12 @@
 package com.example.amir.rehave.others;
 
 import android.content.Context;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -28,9 +29,9 @@ public class CommunityListAdapter extends RecyclerView.Adapter<CommunityListAdap
         TextView nameView;
         TextView dateView;
         TextView timeView;
+        TextView postView;
         TextView commentView;
-        TextView count;
-        RelativeLayout comment;
+        LinearLayout commentLayout;
 
 
         public MyViewHolder(final Context context, final View itemView) {
@@ -38,10 +39,10 @@ public class CommunityListAdapter extends RecyclerView.Adapter<CommunityListAdap
             this.nameView = itemView.findViewById(R.id.name);
             this.dateView = itemView.findViewById(R.id.date);
             this.timeView = itemView.findViewById(R.id.time);
-            this.commentView = itemView.findViewById(R.id.post);
-            this.comment = itemView.findViewById(R.id.comment);
-            this.count = itemView.findViewById(R.id.count);
-            comment.setOnClickListener(new View.OnClickListener() {
+            this.postView = itemView.findViewById(R.id.post);
+            this.commentView = itemView.findViewById(R.id.comment);
+            this.commentLayout = itemView.findViewById(R.id.comment_layout);
+            commentLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
 //                    Toast.makeText(context,"Edit clicked",Toast.LENGTH_SHORT).show();
@@ -49,7 +50,7 @@ public class CommunityListAdapter extends RecyclerView.Adapter<CommunityListAdap
                 }
             });
 
-            commentView.setOnClickListener(new View.OnClickListener() {
+            postView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     CommunityListAdapter.clickListener.onItemClicked(itemView, 1);
@@ -65,7 +66,7 @@ public class CommunityListAdapter extends RecyclerView.Adapter<CommunityListAdap
     public CommunityListAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                              int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.community_item, parent, false);
+                .inflate(R.layout.post_item_list_view, parent, false);
 
 //        view.setOnClickListener(listener);
 
@@ -84,9 +85,9 @@ public class CommunityListAdapter extends RecyclerView.Adapter<CommunityListAdap
         timeView.setText(dataSet.get(listPosition).getTime());
         TextView dateView = holder.dateView;
         dateView.setText(dataSet.get(listPosition).getDate());
-        TextView postView = holder.commentView;
+        TextView postView = holder.postView;
         postView.setText(dataSet.get(listPosition).getPost());
-        TextView countView = holder.count;
+        TextView countView = holder.commentView;
         String count=dataSet.get(listPosition).getCommentCount();
         if (count.equals("0")){
             countView.setVisibility(View.GONE);
