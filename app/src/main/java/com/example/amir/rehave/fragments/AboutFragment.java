9755f95ea8fb8298faf86edbca0855c3524ff191
Amir Hossain <mr.amir.1971@gmail.com>
2019-06-15 12:45:29 +0600

    temp

--- app/src/main/java/com/example/amir/rehave/fragments/AboutFragment.java
+++ app/src/main/java/com/example/amir/rehave/fragments/AboutFragment.java
@@ -1,5 +1,6 @@
 package com.example.amir.rehave.fragments;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.amir.rehave.R;
+import com.example.amir.rehave.databinding.FragmentAboutBinding;
 
 public class AboutFragment extends Fragment {
 
@@ -22,7 +24,10 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        FragmentAboutBinding binding=DataBindingUtil.inflate(
+                inflater, R.layout.fragment_about, container, false);
+
+        return binding.getRoot();
     }
 
 }
